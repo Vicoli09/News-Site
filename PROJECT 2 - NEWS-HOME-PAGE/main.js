0e1bdb6ab94f2fcb4cbb32b071e96cc3d87e2a17
Vicoli09 <127694039+Vicoli09@users.noreply.github.com>
2023-03-25 17:19:51 -0700

    Add files via upload

---
+++ PROJECT 2 - NEWS-HOME-PAGE/main.js
@@ -0,0 +1,9 @@
+const texto = document.querySelector(".news-new");
+
+const btnn = document.querySelector('#description-btn');
+
+
+
+btnn.addEventListener("click", function cambiarColor(){
+    texto.style.color = "blue";
+});
